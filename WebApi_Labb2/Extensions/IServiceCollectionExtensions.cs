e3825c52b5efd233b7db67d2a115ea96313749ef
Felix Cronberg <f.cronberg@gmail.com>
2025-01-16 17:11:57 +0100

    Add IServiceCollectionExtensions + some json I forgot to save

--- WebApi_Labb2/Extensions/IServiceCollectionExtensions.cs
+++ WebApi_Labb2/Extensions/IServiceCollectionExtensions.cs
@@ -1,6 +1,18 @@
-﻿namespace WebApi_Labb2.Extensions
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi_Labb2.Models;
+
+namespace WebApi_Labb2.Extensions
 {
-	public class IServiceCollectionExtensions
+	public static class IServiceCollectionExtensions
 	{
+		public static IServiceCollection ConfigureServicesInMemoryDatabase(this IServiceCollection servColl, IConfiguration configuration)
+		{
+			//Haven't merged yet so BooksDbContext doesn't work yet
+			servColl.AddDbContext<BooksDbContext>(opt => 
+			opt.UseSqlServer(configuration.GetConnectionString("Development")));
+
+			return servColl;
+		}
+
 	}
 }
